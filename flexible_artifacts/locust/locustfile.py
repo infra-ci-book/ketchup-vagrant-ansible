abf0a4ad1a3eaa99b63897a3e16e8da1e01c5eb1
spchildren <spchildren@gmail.com>
2018-05-13 02:50:24 +0900

    updated locust setting

--- flexible_artifacts/locust/locustfile.py
+++ flexible_artifacts/locust/locustfile.py
@@ -1,15 +1,12 @@
 from locust import HttpLocust, TaskSet, task
 
 class KetchupTasks(TaskSet):
-    @task
-    def login(self):
-        self.client.get("/admin/login")
 
     @task
-    def ping(self):
-        self.client.get("/ping")
+    def admin(self):
+        self.client.get("/admin/login")
 
 class WebsiteUser(HttpLocust):
     task_set = KetchupTasks
     min_wait = 500
-    max_wait = 1500
+    max_wait = 1000
