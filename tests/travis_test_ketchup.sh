64f15de4f5112eece03c44f7fc8fb562422ad12f
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2018-09-26 22:39:47 +0900

    removed ansible 2.4.2.0 from release.ansible.com

--- tests/travis_test_ketchup.sh
+++ tests/travis_test_ketchup.sh
@@ -6,15 +6,14 @@ IMAGE="centos:centos7"
 KETCHUP_NAME=$(uuidgen)
 KETCHUP_NGINX_NAME=$(uuidgen)
 TEST_DIR="/tmp/ansible_test_runner"
-ANSIBLE_PACKAGE_URL="https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64"
-ANSIBLE_PACKAGE_NAME="ansible-2.4.3.0-1.el7.ans.noarch.rpm"
+ANSIBLE_VERSION="2.4.2.0"
 
 docker run --detach --privileged --volume="$PWD":$TEST_DIR:rw --name $KETCHUP_NAME $opts $IMAGE /sbin/init
 docker run --detach --privileged --volume="$PWD":$TEST_DIR:rw --name $KETCHUP_NGINX_NAME $opts $IMAGE /sbin/init
 
 # Prepare to launch test playbook
-docker exec $KETCHUP_NAME yum install -y $ANSIBLE_PACKAGE_URL/$ANSIBLE_PACKAGE_NAME
-docker exec $KETCHUP_NGINX_NAME yum install -y $ANSIBLE_PACKAGE_URL/$ANSIBLE_PACKAGE_NAME
+docker exec $KETCHUP_NAME       /bin/bash -c "yum install -y epel-release && yum install -y ansible-${ANSIBLE_VERSION}"
+docker exec $KETCHUP_NGINX_NAME /bin/bash -c "yum install -y epel-release && yum install -y ansible-${ANSIBLE_VERSION}"
 
 # Launch test playbook for ketchup
 docker exec $KETCHUP_NAME       /bin/bash -c "cd $TEST_DIR/tests && ansible-playbook -i travis_test_inventory -t ketchup       travis_test_ketchup.yml"
