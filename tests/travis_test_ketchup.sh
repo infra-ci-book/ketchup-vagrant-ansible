fde0d7d08edaedf114d2b29db0448bcecdcc05b3
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2020-06-11 20:25:12 +0900

    Use version-fixed ansible rpm for testing.

--- tests/travis_test_ketchup.sh
+++ tests/travis_test_ketchup.sh
@@ -7,13 +7,14 @@ KETCHUP_NAME=$(uuidgen)
 KETCHUP_NGINX_NAME=$(uuidgen)
 TEST_DIR="/tmp/ansible_test_runner"
 ANSIBLE_VERSION="2.4.2.0"
+ANSIBLE_RPM="https://github.com/infra-ci-book/support/raw/master/obsoleted/ansible-2.4.2.0-2.el7.noarch.rpm"
 
 docker run --detach --privileged --volume="$PWD":$TEST_DIR:rw --name $KETCHUP_NAME $opts $IMAGE /sbin/init
 docker run --detach --privileged --volume="$PWD":$TEST_DIR:rw --name $KETCHUP_NGINX_NAME $opts $IMAGE /sbin/init
 
 # Prepare to launch test playbook
-docker exec $KETCHUP_NAME       /bin/bash -c "yum install -y epel-release && yum install -y ansible-${ANSIBLE_VERSION}"
-docker exec $KETCHUP_NGINX_NAME /bin/bash -c "yum install -y epel-release && yum install -y ansible-${ANSIBLE_VERSION}"
+docker exec $KETCHUP_NAME       /bin/bash -c "yum install -y epel-release && yum install -y ${ANSIBLE_RPM}"
+docker exec $KETCHUP_NGINX_NAME /bin/bash -c "yum install -y epel-release && yum install -y ${ANSIBLE_RPM}"
 
 # Launch test playbook for ketchup
 docker exec $KETCHUP_NAME       /bin/bash -c "cd $TEST_DIR/tests && ansible-playbook -i travis_test_inventory -t ketchup       travis_test_ketchup.yml"
